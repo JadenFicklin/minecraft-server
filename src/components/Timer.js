9f64ff032f478ebf3d86003a46eb7c70a09b3352
JadenFicklin <jadendficklin@gmail.com>
2022-12-13 11:32:05 -0700

    added in countdown timer

---
+++ src/components/Timer.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Timer({ timerDays, timerHours, timerMinutes, timerSeconds }) {
+  return (
+    <>
+      <section className="timer-container">
+        <section className="timer">
+          <div className="clock">
+            <section>
+              <p>{timerDays}</p>
+              <small>Days</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerHours}</p>
+              <small>Hours</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerMinutes}</p>
+              <small>Minutes</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerSeconds}</p>
+              <small>Seconds</small>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+}
+Timer.defaultProps = {
+  timerDays: 10,
+  timerHours: 10,
+  timerMinutes: 10,
+  timerSeconds: 10,
+};
+export default Timer;
