3c2b539d117e8a46a0506624e6a3896d26efa8ee
JadenFicklin <jadendficklin@gmail.com>
2022-12-17 08:46:47 -0700

    more styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,32 @@ import React from "react";
 function Todo() {
   return (
     <div className="todo-outer">
-      <div className="todo-inner"></div>
+      <div className="todo-inner">
+        <div className="column-one">
+          <h2>Starter list</h2>
+          <input type="checkbox" />
+          <label> I have a bike</label>
+          <br></br>
+          <input type="checkbox" />
+          <label> I have a bike</label>
+        </div>
+        <div className="column-two">
+          <h2>Intermediate list</h2>
+          <input type="checkbox" />
+          <label> I have a bike</label>
+          <br></br>
+          <input type="checkbox" />
+          <label> I have a bike</label>
+        </div>
+        <div className="column-three">
+          <h2>Advanced list</h2>
+          <input type="checkbox" />
+          <label> I have a bike</label>
+          <br></br>
+          <input type="checkbox" />
+          <label> I have a bike</label>
+        </div>
+      </div>
     </div>
   );
 }
